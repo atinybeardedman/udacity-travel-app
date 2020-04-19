c093a82e0cee2f7396db0aec3572fdf596a78f71
Sean Dickinson <sedickinson9123@gmail.com>
2020-04-19 09:10:40 -0400

    feat: added axios

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const cors = require('cors');
-const aylien = require('aylien_textapi');
 const dotenv = require('dotenv');
+const axios = require("axios");
 dotenv.config();
 
+const geoUsername = process.env.GEO_USERNAME;
+const geoNameUrl = 'http://api.geonames.org/postalCodeSearch';
 
-const app = express()
+const weatherBitKey = process.env.WEATHERBIT_KEY;
+
+const pixabayKey = process.env.PIXABAY_KEY;
+
+const app = express();
 
 app.use(express.static('dist'));
 app.use(cors());
@@ -19,4 +25,23 @@ app.get('/', function (req, res) {
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
+})
+
+app.get('/location', (req, resp) => {
+    const placename = req.query.placename;
+    axios.get(geoNameUrl, {
+        params: {
+            username: geoUsername,
+            placename,
+            type: 'json'
+        }
+    }).then(axResp => {
+        console.log(axResp.data);
+        resp.send('Done');
+    }).catch(err => {
+        console.log(err);
+    })
+ 
+
+    
 })
